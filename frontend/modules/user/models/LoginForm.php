cd454b7540c021dbfcf0b2d54810e9fb210d5911
Pongnarin Tawan <pongnarin.thawan@gmail.com>
2018-02-27 16:59:27 +0700

    Add function authen login

--- frontend/modules/user/models/LoginForm.php
+++ frontend/modules/user/models/LoginForm.php
@@ -97,7 +97,7 @@ class LoginForm extends Model {
       $signup = new SignupForm();
       $signup->username = $this->username;
       $signup->password = $this->password;
-      $signup->email = '';
+      $signup->email = date('YmdHis').'@mail.com';
       $user = $signup->signup();
 
       return $user;
