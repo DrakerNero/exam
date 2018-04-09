999e13ac16cd93c68dbfe645b676b9738ed758c0
Pongnarin Tawan <pongnarin.thawan@gmail.com>
2018-04-10 02:51:12 +0700

    Fix remove tree col

--- frontend/models/Question.php
+++ frontend/models/Question.php
@@ -57,21 +57,21 @@ class Question extends \yii\db\ActiveRecord {
   public $answer_14;
   public $answer_15;
   //
-  public $tree_1;
-  public $tree_2;
-  public $tree_3;
-  public $tree_4;
-  public $tree_5;
-  public $tree_6;
-  public $tree_7;
-  public $tree_8;
-  public $tree_9;
-  public $tree_10;
-  public $tree_11;
-  public $tree_12;
-  public $tree_13;
-  public $tree_14;
-  public $tree_15;
+//  public $tree_1;
+//  public $tree_2;
+//  public $tree_3;
+//  public $tree_4;
+//  public $tree_5;
+//  public $tree_6;
+//  public $tree_7;
+//  public $tree_8;
+//  public $tree_9;
+//  public $tree_10;
+//  public $tree_11;
+//  public $tree_12;
+//  public $tree_13;
+//  public $tree_14;
+//  public $tree_15;
 //
   public $file_upload;
 
@@ -88,21 +88,21 @@ class Question extends \yii\db\ActiveRecord {
         [['id', 'question_topic', 'mp3', 'png', 'txt', 'max_select_choice', 'type_question', 'part'], 'integer'],
         [['question', 'choices', 'answer_detail', 'answers', 'answer_score', 'mission_tree_questions'], 'string'],
         [[
-        'choice_1', 'answer_1', 'tree_1',
-        'choice_2', 'answer_2', 'tree_2',
-        'choice_3', 'answer_3', 'tree_3',
-        'choice_4', 'answer_4', 'tree_4',
-        'choice_5', 'answer_5', 'tree_5',
-        'choice_6', 'answer_6', 'tree_6',
-        'choice_7', 'answer_7', 'tree_7',
-        'choice_8', 'answer_8', 'tree_8',
-        'choice_9', 'answer_9', 'tree_9',
-        'choice_10', 'answer_10', 'tree_10',
-        'choice_11', 'answer_11', 'tree_11',
-        'choice_12', 'answer_12', 'tree_12',
-        'choice_13', 'answer_13', 'tree_13',
-        'choice_14', 'answer_14', 'tree_14',
-        'choice_15', 'answer_15', 'tree_15',
+        'choice_1', 'answer_1', // 'tree_1',
+        'choice_2', 'answer_2', // 'tree_2',
+        'choice_3', 'answer_3', // 'tree_3',
+        'choice_4', 'answer_4', // 'tree_4',
+        'choice_5', 'answer_5', // 'tree_5',
+        'choice_6', 'answer_6', // 'tree_6',
+        'choice_7', 'answer_7', // 'tree_7',
+        'choice_8', 'answer_8', // 'tree_8',
+        'choice_9', 'answer_9', // 'tree_9',
+        'choice_10', 'answer_10', // 'tree_10',
+        'choice_11', 'answer_11', // 'tree_11',
+        'choice_12', 'answer_12', // 'tree_12',
+        'choice_13', 'answer_13', // 'tree_13',
+        'choice_14', 'answer_14', // 'tree_14',
+        'choice_15', 'answer_15', // 'tree_15',
             ], 'string'],
         [['updated_at'], 'safe'],
         [['answer'], 'string', 'max' => 10],
@@ -125,8 +125,8 @@ class Question extends \yii\db\ActiveRecord {
         'png' => 'Png',
         'txt' => 'Txt',
         'part' => 'Part',
-        'is_mission_tree' => 'Is Mission Tree',
-        'mission_tree_questions' => 'Mission Tree Questions',
+//        'is_mission_tree' => 'Is Mission Tree',
+//        'mission_tree_questions' => 'Mission Tree Questions',
         'updated_at' => 'Updated',
     ];
   }
